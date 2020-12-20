f98dd08d1a4578a28e0d5dcfdf11f9639b913e15
Rafal Pyska <rafal.pyska1@gmail.com>
2020-12-20 21:55:39 +0100

    Change relation between Posts and Categories

--- client/src/views/Blog.vue
+++ client/src/views/Blog.vue
@@ -7,12 +7,28 @@
 </template>
 
 <script>
+import fetchData from '../mixins/fetchData';
 import BlogPostsList from '../components/Blog/BlogPostsList';
 
 export default {
   name: 'Blog',
   components: {
     BlogPostsList
+  },
+  mixins: [fetchData],
+  data() {
+    return {
+      loading: false,
+      error: '',
+      posts: null
+    };
+  },
+  async mounted() {
+    this.http('http://localhost:1337/posts')
+      .then(data => {
+        this.posts = data;
+        this.loading = false;
+      })
   }
 };
 </script>
