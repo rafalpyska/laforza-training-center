eb9d7039826f003775b6ff90d79f3af8665dfbf3
Rafal Pyska <rafal.pyska1@gmail.com>
2021-01-07 23:40:02 +0100

    Change default color of app in vue.config.js pwa

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   publicPath: '/',
   pwa: {
     name: 'LaForza Training Center',
+    themeColor: '#f83131',
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
