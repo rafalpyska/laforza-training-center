aef16ffa39b85636fb7e7f0021987ba6ec5f557d
Rafal Pyska <rafal.pyska1@gmail.com>
2021-01-19 19:14:09 +0100

    Build for production

---
+++ dist/precache-manifest.9d7cc7c8437e54b8ab43211df2ac2d42.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "26364bf833fc76ec4da9",
+    "url": "/css/about.55a3a373.css"
+  },
+  {
+    "revision": "295646241e2fde82af4e",
+    "url": "/css/app.85323d79.css"
+  },
+  {
+    "revision": "6f2332dc3e35dbcaf85a",
+    "url": "/css/blog.3c422d4a.css"
+  },
+  {
+    "revision": "4108952f97a119f30888",
+    "url": "/css/cart.ada0c435.css"
+  },
+  {
+    "revision": "7578302d1532d4cdc379",
+    "url": "/css/chunk-vendors.eb78aeb9.css"
+  },
+  {
+    "revision": "b59146dd9fb92bbfe968",
+    "url": "/css/classes.89fe6ba2.css"
+  },
+  {
+    "revision": "911525a8a19ed75a2e0c",
+    "url": "/css/contact.67da9a9e.css"
+  },
+  {
+    "revision": "945f3872230657219d21",
+    "url": "/css/events.aa9a6385.css"
+  },
+  {
+    "revision": "4de8f8e6aad15ef88c69",
+    "url": "/css/prices.51d898b9.css"
+  },
+  {
+    "revision": "fbd69789180dc8006d86",
+    "url": "/css/schedule.70c37a61.css"
+  },
+  {
+    "revision": "302f3cbfb32a194dc278",
+    "url": "/css/stories.d712b34c.css"
+  },
+  {
+    "revision": "59643442f0ee1f9226aa",
+    "url": "/css/trainer.27fe17c5.css"
+  },
+  {
+    "revision": "bd173486c6b596d8cf6416f8c0018ea2",
+    "url": "/img/1.bd173486.png"
+  },
+  {
+    "revision": "16e48f0a4c5d3fd7dc4b6c35f092dbc0",
+    "url": "/img/2.16e48f0a.png"
+  },
+  {
+    "revision": "a4af9fc3d25551f133d049f06721012f",
+    "url": "/img/3.a4af9fc3.png"
+  },
+  {
+    "revision": "1544cf1cb8dced7f7a1421295ba2949f",
+    "url": "/img/4.1544cf1c.png"
+  },
+  {
+    "revision": "88d00074d1a80feb7f811c8823812eb7",
+    "url": "/img/5.88d00074.png"
+  },
+  {
+    "revision": "355566697f1513258415ece48b467b13",
+    "url": "/img/6.35556669.png"
+  },
+  {
+    "revision": "4d21c09d8664ada510c9bcbb744d5bc0",
+    "url": "/img/footer-logo.4d21c09d.png"
+  },
+  {
+    "revision": "4f10685d6ef00b3f3bc3d6dfe5b03507",
+    "url": "/img/hero-2.4f10685d.jpg"
+  },
+  {
+    "revision": "89d999ef80c5755665a612fe24542efc",
+    "url": "/img/hero-3.89d999ef.jpg"
+  },
+  {
+    "revision": "56c634ca950a9b84bdc3c542f0346d35",
+    "url": "/img/hero.56c634ca.jpg"
+  },
+  {
+    "revision": "078042a90ae870fa6cda7fe01b867936",
+    "url": "/img/logo-black.078042a9.png"
+  },
+  {
+    "revision": "ff033054ce3ca566b891f3f86f70333e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b042015e668056d789c",
+    "url": "/js/404.f131069b.js"
+  },
+  {
+    "revision": "26364bf833fc76ec4da9",
+    "url": "/js/about.6528d7ee.js"
+  },
+  {
+    "revision": "295646241e2fde82af4e",
+    "url": "/js/app.58ac2d6d.js"
+  },
+  {
+    "revision": "6f2332dc3e35dbcaf85a",
+    "url": "/js/blog.65c2251a.js"
+  },
+  {
+    "revision": "4108952f97a119f30888",
+    "url": "/js/cart.dd1b48f3.js"
+  },
+  {
+    "revision": "7578302d1532d4cdc379",
+    "url": "/js/chunk-vendors.017a7dd5.js"
+  },
+  {
+    "revision": "b59146dd9fb92bbfe968",
+    "url": "/js/classes.c84d5065.js"
+  },
+  {
+    "revision": "911525a8a19ed75a2e0c",
+    "url": "/js/contact.e7222755.js"
+  },
+  {
+    "revision": "945f3872230657219d21",
+    "url": "/js/events.ea0a5011.js"
+  },
+  {
+    "revision": "4de8f8e6aad15ef88c69",
+    "url": "/js/prices.7ad626df.js"
+  },
+  {
+    "revision": "fbd69789180dc8006d86",
+    "url": "/js/schedule.6242683c.js"
+  },
+  {
+    "revision": "302f3cbfb32a194dc278",
+    "url": "/js/stories.cfe5d0e7.js"
+  },
+  {
+    "revision": "59643442f0ee1f9226aa",
+    "url": "/js/trainer.933d5b4a.js"
+  },
+  {
+    "revision": "474f370125dc6ee281cd570a7bf0f870",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
